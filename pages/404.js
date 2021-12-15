6edd088c1f3444cd0347aeb194da8f3a8725cf00
Ad Rienks <ad.rienks@gmail.com>
2021-12-15 19:03:22 +0100

    first commit

---
+++ pages/404.js
@@ -0,0 +1,9 @@
+import Layout from '../components/layout'
+
+export default function Custom404() {
+  return (
+    <Layout>
+      <h1>404 - Deze pagina bestaat helaas niet!</h1>
+    </Layout>
+  )
+}
